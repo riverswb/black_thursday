982d9b3a7947509903cd49ed177c3a8084447929
Brett Rivers <riverswb@gmail.com>
2016-10-31 17:00:45 -0600

    sales engine reading from items and merchants csvs

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,22 +1,34 @@
 require 'csv'
 class SalesEngine
-  attr_reader :items,
-              :merchants
-  def initialize(files)
-    @items = files[:items]
-    @merchants = files[:merchants]
-  end
 
   def self.from_csv(files)
-    contents = CSV.open (files[:items]), headers: true, header_converters: :symbol
+    if files.keys.include?(:items)
+      parse_items(files)
+    elsif files.keys.include?(:merchants)
+      parse_merchants(files)
+    end
+  end
+
+  def self.parse_items(files)
+    contents = CSV.read (files[:items]), headers: true, header_converters: :symbol
     contents.each do |row|
       id = row[:id]
       name = row[:name]
-      description = row[:unit_price]
+      description = row[:description]
+      unit_price = row[:unit_price]
       merchant_id = row[:merchant_id]
       created_at = row[:created_at]
       updated_at = row[:updated_at]
     end
   end
 
+  def self.parse_merchants(files)
+    contents = CSV.read (files[:merchants]), headers: true, header_converters: :symbol
+    contents.each do |row|
+      id = [:id]
+      name = [:name]
+      created_at = [:created_at]
+      updated_at = [:updated_at]
+    end
+  end
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -5,16 +5,21 @@ require './lib/sales_engine'
 
 class SalesEngineTest < Minitest::Test
 
-  # def test_sales_engine_exists
-  #   se = SalesEngine.new({:items =>"./data/items.csv"})
-  #
-  #   assert_equal SalesEngine, se.class
-  # end
-
   def test_sales_engine_reads_from_csv_files
-    se = SalesEngine.from_csv({:items =>"./data/items.csv"})
+    se = SalesEngine.from_csv({:items =>"./data/small/items.csv"})
 
-    assert_equal "", se.items
+    assert_equal "263395237", se[0][:id]
   end
 
+  def test_reads_multiple_merchants_and_rows_from_csv_file
+    se = SalesEngine.from_csv({:items =>"./data/small/items.csv"})
+
+    assert_equal "Glitter scrabble frames", se[1][:name]
+  end
+
+  def test_reads_from_merchant_csv_files
+    se = SalesEngine.from_csv({:merchants => "./data/merchants.csv"})
+
+    assert_equal "12334105", se[0][:id]
+  end
 end
