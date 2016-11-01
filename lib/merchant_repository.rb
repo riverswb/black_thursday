8c2384124ce666710b1712e7c53509d79dca6b48
Daleon <rugfia@yahoo.com>
2016-11-01 08:28:41 -0600

    First Commit - I0

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -1,4 +1,24 @@
+require_relative "../lib/merchant"
+require "csv"
+require 'pry'
+
+
 class MerchantRepository
+attr_reader :all
 
+  def initialize(data_path, sales_engine=nil)
+    @all = []
+    csv_loader(data_path)
+    merchant_maker
+  end
 
+  def csv_loader(data_path)
+    @csv = CSV.open data_path, headers:true, header_converters: :symbol
+  end
+
+  def merchant_maker
+    @all = @csv.map do |row|
+      Merchant.new(row, self)
+    end
+  end
 end
