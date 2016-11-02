7c0d97269cdb4c921e924c377bb967ff532ecae4
Brett Rivers <riverswb@gmail.com>
2016-11-02 08:22:01 -0600

    std deviation working

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,36 +1,39 @@
 class SalesAnalyst
-  attr_reader :se
+  attr_reader :se,
+              :merchants_items,
+              :average
   def initialize(sales_engine)
     @se = sales_engine
+    @merchants_items = {}
+    @average = average
   end
 
   def average_items_per_merchant
       items = se.items.all.count.to_f
       merchants = se.merchants.all.count.to_f
-      (items / merchants).round(2)
+      @average = (items / merchants)
+      average.round(2)
   end
 
 
   def items_per_merchant
-    items = se.items.all
     merchants = se.merchants.all
-    merchants_items =  {}
     merchants.map do |merchant|
       merchants_items.store(merchant.id, se.items.find_all_by_merchant_id(merchant.id))
     end
   end
-  # def average_items_per_merchant_standard_deviation
-  #   # so I've been doing this completely wrong
-  #   items = se.items.all.count.to_f
-  #   merchants = se.merchants.all.count.to_f
-  #   mean = (items / merchants)
-  #   items_index = []
-  #   se.items.all.each_with_index do |item, index|
-  #     items_index << index
-  #   end
-  #   step_1 = items_index.map do |item|
-  #     (item - mean) ** 2
-  #     end
-  #   end
+
+  def average_items_per_merchant_standard_deviation
+    average_items_per_merchant
+    number_items_per_merchant = @merchants_items.map do |merchant|
+      merchant[1].count
+    end
+    step_1 = number_items_per_merchant.map do |num|
+      (num - average) ** 2
+    end
+    step_2 = step_1.reduce(:+)
+    step_3 = step_2 / se.merchants.all.count - 1
+    step_4 = Math.sqrt(step_3)
+  end
 
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -7,8 +7,8 @@ class SalesAnalystTest < Minitest::Test
   attr_reader :se
   def setup
     @se = SalesEngine.from_csv({
-      :items     => "./data/items.csv",
-      :merchants => "./data/merchants.csv"
+      :items     => "./data/small/items.csv",
+      :merchants => "./data/small/merchants.csv"
     })
   end
 
@@ -31,14 +31,25 @@ class SalesAnalystTest < Minitest::Test
 
   def test_can_find_the_number_of_items_for_each_merchant
     sa = SalesAnalyst.new(se)
-
-    assert_equal [], sa.items_per_merchant
+    sa.items_per_merchant
+    assert_equal 1, sa.merchants_items[12334141].count
   end
 
   def test_sa_can_calculate_average_items_per_merchant_standard_deviation
-    skip
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
     sa = SalesAnalyst.new(se)
-    assert_equal 0, sa.average_items_per_merchant_standard_deviation
+    sa.items_per_merchant
+    assert_equal 3.0983587827774977, sa.average_items_per_merchant_standard_deviation
   end
+
+  def merchants_with_high_item_count
+    se = SalesEngine.from_csv({
+      :items     => "./data/items.csv",
+      :merchants => "./data/merchants.csv"
+    })
+    sa = SalesAnalyst.new(se)
 
 end
